57db8aa45fa52cbca1b32bb7f1cb5c747eb3ee21
James Richters <james@productionautomation.net>
2016-07-23 06:48:16 -0400

    Fix external declarations to use C library
    See link here:  http://www.freepascal.org/docs-html/ref/refsu72.html

--- inpout32.pas
+++ inpout32.pas
@@ -1,33 +1,33 @@
 unit inpout32;
 
 interface
-Function IsInpOutDriverOpen:boolean;
-Function IsXP64Bit:boolean;
+Function IsInpOutDriverOpen:boolean;{$ifndef Win32} cdecl {$else} stdcall {$endif};
+Function IsXP64Bit:boolean;{$ifndef Win32} cdecl {$else} stdcall {$endif};
 
-Function  Inp32(Port:SmallInt):Byte;
-Procedure Out32(Port:SmallInt; Value:Byte);
+Function  Inp32(Port:longint):Byte;{$ifndef Win32} cdecl {$else} stdcall {$endif};
+Procedure Out32(Port:longint; Value:Byte);{$ifndef Win32} cdecl {$else} stdcall {$endif};
 
-Function DlPortReadPortUchar(Port:SmallInt):Byte;
-Function DlPortReadPortUshort(Port:SmallInt):Word;
-Function DlPortReadPortUlong(Port:SmallInt):Cardinal;
+Function DlPortReadPortUchar(Port:longInt):Byte;{$ifndef Win32} cdecl {$else} stdcall {$endif};
+Function DlPortReadPortUshort(Port:longInt):Word;{$ifndef Win32} cdecl {$else} stdcall {$endif};
+Function DlPortReadPortUlong(Port:longInt):Cardinal;{$ifndef Win32} cdecl {$else} stdcall {$endif};
 
-Procedure DlPortWritePortUchar(Port:SmallInt; Value:Byte);
-Procedure DlPortWritePortUshort(Port:SmallInt; Value:Word);
-Procedure DlPortWritePortUlong(Port:SmallInt; Value:Cardinal);
+Procedure DlPortWritePortUchar(Port:longInt; Value:Byte);{$ifndef Win32} cdecl {$else} stdcall {$endif};
+Procedure DlPortWritePortUshort(Port:longInt; Value:Word);{$ifndef Win32} cdecl {$else} stdcall {$endif};
+Procedure DlPortWritePortUlong(Port:longInt; Value:Cardinal);{$ifndef Win32} cdecl {$else} stdcall {$endif};
 
 implementation
-Function IsInpOutDriverOpen; external 'inpout32.DLL';
-Function IsXP64Bit; external 'inpout32.DLL';
+Function IsInpOutDriverOpen; {$ifndef Win32} cdecl {$else} stdcall {$endif}; external 'inpout32.DLL';
+Function IsXP64Bit; {$ifndef Win32} cdecl {$else} stdcall {$endif}; external 'inpout32.DLL';
 
-Function Inp32; external 'inpout32.DLL';
-Procedure Out32; external 'inpout32.DLL';
+Function Inp32; {$ifndef Win32} cdecl {$else} stdcall {$endif}; external 'inpout32.DLL';
+Procedure Out32; {$ifndef Win32} cdecl {$else} stdcall {$endif}; external 'inpout32.DLL';
 
-Function DlPortReadPortUchar; external 'inpout32.DLL';
-Function DlPortReadPortUshort; external 'inpout32.DLL';
-Function DlPortReadPortUlong; external 'inpout32.DLL';
+Function DlPortReadPortUchar; {$ifndef Win32} cdecl {$else} stdcall {$endif}; external 'inpout32.DLL';
+Function DlPortReadPortUshort; {$ifndef Win32} cdecl {$else} stdcall {$endif}; external 'inpout32.DLL';
+Function DlPortReadPortUlong; {$ifndef Win32} cdecl {$else} stdcall {$endif}; external 'inpout32.DLL';
 
-Procedure DlPortWritePortUchar; external 'inpout32.DLL';
-Procedure DlPortWritePortUshort; external 'inpout32.DLL';
-Procedure DlPortWritePortUlong; external 'inpout32.DLL';
+Procedure DlPortWritePortUchar; {$ifndef Win32} cdecl {$else} stdcall {$endif}; external 'inpout32.DLL';
+Procedure DlPortWritePortUshort; {$ifndef Win32} cdecl {$else} stdcall {$endif}; external 'inpout32.DLL';
+Procedure DlPortWritePortUlong; {$ifndef Win32} cdecl {$else} stdcall {$endif}; external 'inpout32.DLL';
 end.
 
